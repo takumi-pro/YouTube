054da77b608b8476c14f94213f20ea42bec30689
takumi-pro <takumidiary.0927@gmail.com>
2021-05-16 16:32:22 +0900

    sass導入

--- index.php
+++ index.php
@@ -5,51 +5,70 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./dist/app.css">
     <title>YouTube</title>
 </head>
 
 <body>
-    <form action="">
-        <input class="search" type="text" name="name">
-        <button class="search_btn">検索</button>
-    </form>
+    <header class="header">
+        <form class="search_form" action="">
+            <input class="search" type="text" name="name">
+            <button class="search_btn">検索</button>
+        </form>
+    </header>
 
     <div class="info">
 
     </div>
 </body>
 
-<script src="https://apis.google.com/js/client.js" type="text/javascript"></script>
 <script>
 const input = document.querySelector('.search');
 const btn = document.querySelector('.search_btn');
 const info = document.querySelector('.info');
 
-const key = '?key=AIzaSyAEd1TOfXFjpao-MoligM0MwdbPMyouIrQ';
-let url = 'https://www.googleapis.com/youtube/v3/search';
+const API_KEY = '&key=AIzaSyAEd1TOfXFjpao-MoligM0MwdbPMyouIrQ';
+const baseUrl = 'https://www.googleapis.com/youtube/v3';
 let maxResults = '&maxResults=10';
-url += key + '&part=snippet' + maxResults;
-// url += '?part=snippet';
+
+//検索URLの生成
+let searchSnippet = '?part=snippet';
+let searchUrl = baseUrl + '/search' + searchSnippet + maxResults + API_KEY;
+
+//プレイリストの生成
+let subscSnippet = '?part=snippet';
+let playlistId = '&playlistId=PLBJsdIdy3xPtFcpakhCiybjNiMFpG0-BK';
+let playlistUrl = baseUrl + '/playlistItems' + subscSnippet + playlistId + API_KEY;
 
 window.onload = function() {
     btn.onclick = fetchData;
-}
 
-function fetchData(e) {
-    e.preventDefault();
-    let val = input.value;
-    url += '&q=' + val;
-
-    fetch(url, {
+    fetch(playlistUrl, {
         mode: 'cors'
     }).then(function(response) {
         return response.json();
     }).then(function(json) {
-        createInfo(json);
+        create(json);
     })
 }
 
-function createInfo(json) {
+function fetchData(e) {
+    e.preventDefault();
+
+    let val = input.value;
+    searchUrl += '&q=' + val;
+    console.log(searchUrl);
+
+    fetch(searchUrl, {
+        mode: 'cors'
+    }).then(function(response) {
+        return response.json();
+    }).then(function(json) {
+        create(json);
+    })
+}
+
+function create(json) {
     while (info.firstChild) {
         info.removeChild(info.firstChild);
     }
